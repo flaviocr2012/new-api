a50c36d7116548a68285d038a58ecb5151de79c6
flavio <flaviocr2012@gmail.com>
2021-09-27 20:43:01 -0300

    Initial commit. Criando os métodos no controller.

---
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -0,0 +1,51 @@
+package com.example.demo.api;
+
+import com.example.demo.model.Person;
+import com.example.demo.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("/api/v1/person")
+@RestController
+public class PersonController {
+
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    public void addPerson(Person person) {
+        personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople() {
+        return personService.getAllPeople();
+    }
+
+    @GetMapping(path = "{id}")
+    public Person getPersonById(UUID id) {
+        return personService.getPersonById(id)
+                .orElse(null);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deletePersonById(UUID id) {
+        personService.deletePerson(id);
+    }
+
+    public void updatePerson(UUID id, Person person) {
+        personService.updatePerson(id, person);
+    }
+
+
+
+
+
+}
---
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Person;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class PersonService {
+    public void addPerson(Person person) {
+
+    }
+
+    public List<Person> getAllPeople() {
+        return null;
+    }
+
+    public Optional<Person> getPersonById(UUID id) {
+        return null;
+    }
+
+    public void deletePerson(UUID id) {
+
+    }
+
+    public void updatePerson(UUID id, Person person) {
+
+    }
+}
