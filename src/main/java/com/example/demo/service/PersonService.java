56535ca56cb168df69ec11d2ea2bd68f30bc74f3
flavio <flaviocr2012@gmail.com>
2021-09-27 21:45:37 -0300

    Criando PostgresDataSource

--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -4,11 +4,13 @@ import com.example.demo.dao.PersonDAO;
 import com.example.demo.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Service
 public class PersonService {
 
     private final PersonDAO personDAO;
